736e96d043645622ab7e8d5210ac943f01524f8d
robertrivas <r_rivas@csu.fullerton.edu>
2020-11-06 12:55:03 -0800

    Changed android app name to company name on new branch to not break master branch, incase.

--- ios/EquipmentDirectTests/EquipmentDirectTests.m
+++ ios/EquipmentDirectTests/EquipmentDirectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNWCShopTests : XCTestCase
+@interface EquipmentDirectTests : XCTestCase
 
 @end
 
-@implementation RNWCShopTests
+@implementation EquipmentDirectTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
